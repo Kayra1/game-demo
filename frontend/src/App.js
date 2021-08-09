dabdf2c40a8e811b2fb0918bdab0feaf3a9bc4e4
Kayra1 <kayra959@gmail.com>
2021-08-09 16:20:13 -0700

    redux simple boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route} from "react-router-dom"
 import Login from "./components/Login"
 
 function App() {
+
   return (
-    <Router>
-      <div>
-        Hello World
-      </div>
-      <Route exact path="/login" component={Login}/>
-    </Router>
+      <Router>
+        <div>
+          Hello World
+        </div>
+        <Route exact path="/login" component={Login}/>
+      </Router>
   );
 }
 
---
+++ frontend/src/actions/authActions.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import {
+    GET_ERRORS,
+    USER_LOADING,
+    SET_CURRENT_USER
+} from "./types"
+
+// Action to log user in
+export const loginUser = userData => dispatch => {
+    axios.post("/login", userData)
+        .then(res => {
+            // Save token for use in axios
+            const {token} = res.data
+            localStorage.setItem("jwtToken", token);
+            setAuthToken(token)
+
+            // Decode for user data
+            const decoded = jwt_decode(token)
+            dispatch(setCurrentUser(decoded))
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
+}
+
+// Setting logged in user
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
+
+// Setting loading user
+export const setUserLoading = () => {
+    return {
+        type: USER_LOADING
+    }
+}
+
+// Logging user out (no need to do anything serverside)
+export const logoutUser = () => {
+    localStorage.removeItem("jwtToken")
+    setAuthToken(false)
+    dispatch(setCurrentUser({}))
+}
